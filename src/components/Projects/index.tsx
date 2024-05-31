5fcfcf5003cc998217580912970fefd5cb1a48fc
Spyei <caio.salamao1234@gmail.com>
2024-05-30 23:28:55 -0300

    feat: inital project page

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,18 @@
+import Project from "./Project";
+
+export default function ProjectsComponent() {
+    return (
+        <section className="w-screen flex justify-center">
+            <div className="max-w-[800px] w-full flex pt-16 mobile:pt-6">
+                <div className="flex gap-3 flex-col tablet:px w-full">
+                    <h1 className="font-bold text-3xl mobile:text-xl">Projetos</h1>
+                    <div>Projetos feitos por mim mesmo, ou ainda em desenvolvimento.</div>
+                    <div className="w-full flex flex-col gap-4">
+                        <Project description="Simo botlist é um site que divulga bots de discord" image="/simo/page.png" langs={["react", "tailwind", "ts"]} name="Simo" key={Math.random()} />
+                        <Project name="Postboy" langs={["next", "tailwind", "ts"]} description="Postboy é um site que faz requisições http para um url." image="/postboy/page.png" key={Math.random()} />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
