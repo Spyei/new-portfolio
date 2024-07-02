4713348ab74d0e1470e6f00dcc6d4d27037a7a77
Spyei <caio.salamao1234@gmail.com>
2024-07-02 17:12:57 -0300

    feat: theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,21 @@ import { AppProps } from 'next/app';
 import SideBar from "@/components/SideBar";
 import { Inter } from "next/font/google";
 import { HeroHighlight } from "@/components/ui/hero-highlight";
+import DarkMode from "@/components/Mixed/DarkMode";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
     return (
-        <HeroHighlight>
-            <section className={`${inter.className} overflow-x-hidden flex text-neutral-700 min-h-screen`}>
-                <SideBar />
-                <Component {...pageProps} />
-            </section>
-        </HeroHighlight>
+        <ThemeProvider attribute="class">
+            <HeroHighlight>
+                <section className={`${inter.className} overflow-x-hidden flex text-neutral-700 dark:text-neutral-300 min-h-screen`}>
+                    <SideBar />
+                    <Component {...pageProps} />
+                </section>
+            </HeroHighlight>
+        </ThemeProvider>
     );
 };
 
