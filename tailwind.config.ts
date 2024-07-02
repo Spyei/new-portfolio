84050913d714b816614b340958645fad5db51ffb
Spyei <caio.salamao1234@gmail.com>
2024-07-02 17:10:13 -0300

    feat: darkmode class to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const colors = require("tailwindcss/colors");
 const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
+    darkMode: "class",
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
