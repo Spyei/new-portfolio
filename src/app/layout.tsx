f9e54ac2d3bf1305c21ad66305e6c7b6ddc99aac
Spyei <caio.salamao1234@gmail.com>
2024-07-02 17:41:07 -0300

    fix: layout hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,21 @@ import "./globals.css";
 import SideBar from "@/components/SideBar";
 import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
+import { HeroHighlight } from "@/components/ui/hero-highlight";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
     return (
         <html lang="pt-br">
-            <ThemeProvider attribute="class">
-                <body className={inter.className}>
-                    <SideBar />
-                    {children}
-                </body>
-            </ThemeProvider>
+            <HeroHighlight>
+                <ThemeProvider attribute="class">
+                    <body className={inter.className}>
+                        <SideBar />
+                        {children}
+                    </body>
+                </ThemeProvider>
+            </HeroHighlight>
         </html>
     );
 }
