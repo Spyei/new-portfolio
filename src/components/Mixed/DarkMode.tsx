9cee2906f4f7d5261f129ba2892b83371c6fa7d6
Spyei <caio.salamao1234@gmail.com>
2024-07-02 17:12:39 -0300

    feat: darkmode button

---
+++ src/components/Mixed/DarkMode.tsx
@@ -0,0 +1,39 @@
+import { useTheme } from "next-themes";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+export default function DarkMode() {
+    const { theme, setTheme } = useTheme();
+    const [isDark, setIsDark] = useState(false);
+
+    const handleThemeChange = () => {
+        setIsDark(!isDark);
+
+        return theme === "dark" ? setTheme("light") : setTheme("dark");
+    };
+
+    return (
+        <button
+            onClick={handleThemeChange}
+            className="bg-neutral-100 dark:bg-neutral-900 shadow-sm shadow-neutral-900 rounded-full w-full p-3 tablet:p-2 tablet:w-auto relative tablet:mb-1"
+        >
+            <div className="tablet:hidden">
+                <motion.span
+                    animate={{ x: isDark ? 16 : -14 }}
+                    className={`absolute top-[14px] bottom-0 left-0 right-0`}
+                >
+                    {!isDark ? "Modo escuro" : "Modo claro"}
+                </motion.span>
+                <motion.div animate={{ x: isDark ? 0 : 110 }}>
+                    {isDark ? <MdLightMode className="size-7" /> : <MdDarkMode className="size-7" />}
+                </motion.div>
+            </div>
+            <div className="tabletmin:hidden">
+                <motion.div animate={{ rotate: isDark ? 260 : 0 }}>
+                    {isDark ? <MdLightMode className="size-7" /> : <MdDarkMode className="size-7" />}
+                </motion.div>
+            </div>
+        </button>
+    )
+}
