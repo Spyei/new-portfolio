a17a7e63dfeca5dd88568d76f4c75fdee6cdceee
Spyei <caio.salamao1234@gmail.com>
2024-07-02 18:16:12 -0300

    fix: age

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,29 @@ import Head from "next/head";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const [age, setAge] = useState(0);
+
+    useEffect(() => {
+        const calculateAge = () => {
+            const birthdate = new Date(2007, 8, 8);
+            const today = new Date();
+            let age = today.getFullYear() - birthdate.getFullYear();
+            const monthDiff = today.getMonth() - birthdate.getMonth();
+            const dayDiff = today.getDate() - birthdate.getDate();
+
+            if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+                age--;
+            }
+
+            return age;
+        };
+
+        setAge(calculateAge());
+    }, []);
+
     return (
         <>
             <Head>
@@ -28,7 +49,7 @@ export default function Home() {
                     <div className="flex gap-3 flex-col tablet:px-6">
                         <h1 className="font-bold text-3xl mobile:text-xl">Olá! Meu nome é Caio 👋</h1>
                         <div className="flex gap-1 flex-col">
-                            <p>Tenho 17 anos, e sou um <Highlight>Desenvolvedor Front-end</Highlight> apaixonado por programação, tenho <strong>1 ano de experiência</strong> na área, fazendo freelances e projetos pessoais.</p>
+                            <p>Tenho {age} anos, e sou um <Highlight>Desenvolvedor Front-end</Highlight> apaixonado por programação, tenho <strong>1 ano de experiência</strong> na área, fazendo freelances e projetos pessoais.</p>
                             <p>Estou constantemente em busca de aprendizado e aprimoramento dos meus conhecimentos, sempre tentando melhorar a cada dia!</p>
                         </div>
                         <Stacks />
