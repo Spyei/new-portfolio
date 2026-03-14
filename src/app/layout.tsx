fc99f743902d5142044a51a8b0f71bbfa48b6b97
Spyei <caio.salamao1234@gmail.com>
2026-03-14 13:38:32 -0400

    fix: tailwindcss cache

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 import "./globals.css";
 import type { Metadata } from "next";
 import Sidebar from "@/components/sidebar";
+import Head from "next/head";
 
 export const metadata: Metadata = {
 	title: "Portfolio",
@@ -15,11 +16,23 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="pt-BR">
+			<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="anonymous"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Familjen+Grotesk:wght@400;500;600;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
 			<body
 				className={`font-sans antialiased bg-background text-secondary dark`}
 			>
 				<Sidebar />
-				<main className="ml-56 flex-1 p-4">
+				<main className="md:ml-56 ml-16 flex-1 p-4">
 					<div className="max-w-3xl mx-auto">{children}</div>
 				</main>
 			</body>
