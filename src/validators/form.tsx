0bdd12391c7a9ff2e72c4dac039094326fe874b7
Spyei <caio.salamao1234@gmail.com>
2026-03-15 17:00:56 -0400

    fix: email form validator

--- src/validators/form.tsx
+++ src/validators/form.tsx
@@ -19,16 +19,22 @@ export function validateContactForm(data: ContactFormData): ContactFormErrors {
 
 	if (!name.trim()) {
 		errors.name = "Lembre-se de preencher o nome";
+	} else if (name.length > 50) {
+		errors.name = "O nome deve conter no máximo 50 caracteres";
 	}
 
 	if (!email.trim()) {
 		errors.email = "Lembre-se de preencher o e-mail";
 	} else if (!EMAIL_REGEX.test(email)) {
 		errors.email = "Certifique-se de fornecer um e-mail válido";
+	} else if (email.length > 255) {
+		errors.email = "O e-mail deve conter no máximo 255 caracteres";
 	}
 
 	if (!message.trim()) {
 		errors.message = "Lembre-se de preencher a mensagem";
+	} else if (message.length > 500) {
+		errors.message = "A mensagem deve conter no máximo 500 caracteres";
 	}
 
 	return errors;
