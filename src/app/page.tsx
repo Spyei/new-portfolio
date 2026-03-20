79eaa7300a83eeae63f33561f3f24497ed52bf9e
Spyei <caio.salamao1234@gmail.com>
2026-03-20 02:46:05 -0400

    fix: force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+export const dynamic = "force-dynamic";
+
 import HomeContent from "@/components/home";
 import Container from "@/components/ui/container";
 import { getCarbonData } from "@/lib/carbonApi";
+
+export const revalidate = 86400;
 
 export default async function Home() {
 	const carbon = await getCarbonData();
