687fe7109fe2e75721f255b61cae6a460d9d2e4e
Spyei <caio.salamao1234@gmail.com>
2026-03-25 01:03:07 -0400

    feat: new translations and scrollbar

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,54 @@ const nextConfig: NextConfig = {
 				protocol: "https",
 				hostname: "media.discordapp.net",
 			},
+			{
+				protocol: "https",
+				hostname: "s4.anilist.co",
+			},
+			{
+				protocol: "https",
+				hostname: "lastfm.freetls.fastly.net",
+			},
+			{
+				protocol: "https",
+				hostname: "i.imgur.com",
+			},
+			{
+				protocol: "https",
+				hostname: "lastfm.freetls.fastly.net",
+			},
+			{
+				protocol: "https",
+				hostname: "*.fastly.net",
+			},
+			{
+				protocol: "https",
+				hostname: "i.imgur.com",
+			},
+			{
+				protocol: "https",
+				hostname: "is1-ssl.mzstatic.com",
+			},
+			{
+				protocol: "https",
+				hostname: "is2-ssl.mzstatic.com",
+			},
+			{
+				protocol: "https",
+				hostname: "is3-ssl.mzstatic.com",
+			},
+			{
+				protocol: "https",
+				hostname: "is4-ssl.mzstatic.com",
+			},
+			{
+				protocol: "https",
+				hostname: "is5-ssl.mzstatic.com",
+			},
+			{
+				protocol: "https",
+				hostname: "*.mzstatic.com",
+			},
 		],
 	},
 };
