44cd4fe3aa1ca204e3cf7b69f34a23802a262a4f
Spyei <caio.salamao1234@gmail.com>
2026-03-25 01:02:27 -0400

    feat: format number util

---
+++ src/utils/formatNumber.ts
@@ -0,0 +1,24 @@
+const lookup = [
+	{ value: 1, symbol: "" },
+	{ value: 1e3, symbol: "k" },
+	{ value: 1e6, symbol: "M" },
+	{ value: 1e9, symbol: "G" },
+	{ value: 1e12, symbol: "T" },
+	{ value: 1e15, symbol: "P" },
+	{ value: 1e18, symbol: "E" },
+];
+
+export function formatNumber(num: number, digits: number = 2) {
+	const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+
+	var item = lookup
+		.slice()
+		.reverse()
+		.find((item) => {
+			return num >= item.value;
+		});
+
+	return item
+		? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol
+		: "0";
+}
